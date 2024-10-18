5323a669b53cabaedd64e42d4eb3d541752886c0
Mateusz Dzieżok <mat.dziezok@gmail.com>
2024-10-18 16:29:27 +0000

    fixed opening files

--- upload_s3_python.py
+++ upload_s3_python.py
@@ -28,6 +28,7 @@ with open(ec2InsDatafile, 'w') as fh:
         try: 
             response = requests.get(meta_data +'/' + value)
             print(response.text)
+            ec2_params[param]=response.text
 
         except requests.RequestException as e:
             print(f"Error while making request {param}: {e}")
@@ -45,10 +46,10 @@ with open(ec2InsDatafile, 'w') as fh:
     #Getting  OS related if from system files
     os_vers = "grep '^VERSION=' /etc/os-release |cut -d'=' -f2"
     os_name = "grep '^NAME' /etc/os-release |cut -d'=' -f2"
-    os_name_val ='OS NAME: '+ os.popen(os_name).read().rstrip()
-    os_vers_val ='OS VERSION: '+ os.popen(os_vers).read().rstrip()
-    os_usrs = "grep -E 'bash|sh' /etc/passwd |awk -F : '{print $1}|xargs echo  "
-    os_usrs_val = 'Login able users: '+ os.popen(os_usrs).read().rstrip()
+    os_name_val ="OS NAME: "+ os.popen(os_name).read().rstrip()
+    os_vers_val ="OS VERSION: "+ os.popen(os_vers).read().rstrip()
+    os_usrs = "grep -E 'bash|sh' /etc/passwd |awk -F : '{print $1}'|xargs echo  "
+    os_usrs_val = "Login able users: " + os.popen(os_usrs).read().rstrip()
     try:
         fh.write(os_name_val+'\r\n')
         fh.write(os_vers_val+'\r\n')
@@ -57,25 +58,29 @@ with open(ec2InsDatafile, 'w') as fh:
         print("Error during write to file")
 
 
+#print(ec2_params)
+
+print("uploading to bucket")
 # Upload file to s3 storage
-s3_bucket_name = 'new-bucket-e05ab0e0'
+#s3_bucket_name = 'new-bucket-e05ab0e0'
+s3_bucket_name = 'applicant-task'
+s3_upload_path = 'r5d4/'
 s3_conn = boto3.client('s3')
 
 try:
-    bckt_head = s3_conn.meta.client.head_bucket(Bucket=s3_bucket_name)
-    print(bckt_head)
-    instance_id = ec2_params.get('Instance ID')
-    print(instance_id)
+    # no permissions
+    #bckt_head = s3_conn.head_bucket(Bucket=s3_bucket_name)
+    #print("bckt_head = " + bckt_head)
 
-    with (ec2InsDatafile, 'r') as fh:
+    with open(ec2InsDatafile, 'r') as fh2:
         #https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/put_object.html
-        response  = s3_conn.put_object(
-            Bucket=s3_bucket_name,
-            Key='system_info' + requests.get(meta_data +'/' + instance_id) + '.txt',
-            Body=fh.read()
+        response = s3_conn.put_object(
+            Bucket = s3_bucket_name,
+            Key = s3_upload_path + "system_info" + ec2_params['Instance ID'] + ".txt",
+            Body = fh2.read()
         )
         print(response)
-    print("File has been uploaded into " + s3_bucket_name + " S3 bucket with instance_id key.")
+    print("File has been uploaded into " + s3_bucket_name + "/" + s3_upload_path + " S3 bucket with instance_id key.")
 except ClientError as e:
     print(e.response['Error']['Message'])
     print("Are you sure the destination bucket exist? Check it.")
